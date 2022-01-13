0d4c6f36f2f7a26ad68f93e4995a87639ffe3ad4
Narges <narges.moharrami97@gmail.com>
2022-01-13 08:57:32 -0800

    classes added

---
+++ main.py
@@ -0,0 +1,17 @@
+ALLOWED_EXTENSION = ["html", "csv", "mp3", "mp4", "txt"]
+
+
+class AbstractRender:
+    pass
+
+
+class HtmlRender:
+    pass
+
+
+class MP3Render:
+    pass
+
+
+class MP4Render:
+    pass
