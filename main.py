12489b8976beebe893f2fc01b18098643bfcf509
Narges <narges.moharrami97@gmail.com>
2022-01-14 18:36:25 -0800

    FileHandler class added

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
+from abc import ABC, abstractmethod
+
 ALLOWED_EXTENSION = ["html", "csv", "mp3", "mp4", "txt"]
 
 
-class AbstractRender:
-    pass
+class AbstractRender(ABC):
+    @abstractmethod
+    def render(self):
+        pass
 
 
-class HtmlRender:
-    pass
+class HtmlRender(AbstractRender):
+    def render(self):
+        print("Render using HtmlRender")
 
 
-class MP3Render:
-    pass
+class MP3Render(AbstractRender):
+    def render(self):
+        print("Render using MP3Render")
 
 
-class MP4Render:
-    pass
+class MP4Render(AbstractRender):
+    def render(self):
+        print("Render using MP4Render")
+
+
+class FileHandler:
+    def __init__(self, filename):
+        self.filename = filename
+
+    @classmethod
+    def create(cls, filename):
+        if filename.split(".") not in ALLOWED_EXTENSION:
+            print("file extension not allowed")
+        return filename
