369f0dd44850ac74129d439abf7a73fa826bcdf5
Narges <narges.moharrami97@gmail.com>
2022-01-14 19:11:16 -0800

    Render HTML_file and MP3_file and MP4_file.

--- main.py
+++ main.py
@@ -25,11 +25,36 @@ class MP4Render(AbstractRender):
 
 
 class FileHandler:
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, file_name):
+        self.file_name = file_name
 
     @classmethod
     def create(cls, filename):
-        if filename.split(".") not in ALLOWED_EXTENSION:
+        if filename.split(".")[-1] not in ALLOWED_EXTENSION:
             print("file extension not allowed")
-        return filename
+        return cls(filename)
+
+    @property
+    def extension(self):
+        return self.file_name.split(".")[-1]
+
+    def render(self):
+        handler_dict = {
+            "html": HtmlRender,
+            "mp3": MP3Render,
+            "mp4": MP4Render
+        }
+        handler = handler_dict[self.extension]
+        return handler().render()
+
+
+if __name__ == "__main__":
+    f1 = FileHandler.create("document.pdf")
+    f2 = FileHandler.create("document.html")
+    f3 = FileHandler.create("movie.mp3")
+    f4 = FileHandler.create("movie.mp4")
+
+    file_list = [f2, f3, f4]
+
+    for filename in file_list:
+        filename.render()
